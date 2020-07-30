25681c9c038f9f2876a057b1a70a2a4ab4215e1c
xueqing.cao <xueqing.cao@greatmicro.net>
2020-07-30 15:38:11 +0800

    第一次提交

---
+++ vue-demo/src/components/sub/Confirm.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="confirm">
+      <el-button @click="getButtonClick">
+          {{text || '确认'}}
+      </el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Confirm',
+  props: ['text'],
+  data () {
+    return {
+      msg: true
+    }
+  },
+  methods: {
+    getButtonClick () {
+      this.$emit('message', this.msg)
+    }
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+$color: red;
+.main {
+  color: $color;
+}
+
+</style>
---
+++ vue-demo/src/components/views/First.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="main">
+    main part
+    <confirm text="注册" @message="getMessage"></confirm>
+  </div>
+</template>
+
+<script>
+import Confirm from '../sub/Confirm'
+export default {
+  name: 'First',
+  components: {
+    Confirm
+  },
+  data () {
+    return {
+      msg: 'Welcome to FirstApp',
+      isCollapse: true
+    }
+  },
+  methods: {
+    getMessage (val) {
+      alert(val)
+    }
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+$color: red;
+.main {
+  color: $color;
+}
+
+</style>
